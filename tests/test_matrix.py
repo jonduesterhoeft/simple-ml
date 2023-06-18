d428b45cce481b08cb147eb505fa3a957a423a80
Jon Duesterhoeft <jon.duesterhoeft@gmail.com>
2023-06-18 09:22:03 -0500

    updated directories, added linear algebra tests

---
+++ tests/test_matrix.py
@@ -0,0 +1,120 @@
+import pytest
+import math
+
+from data_tools.linear_algebra import matrix as m
+
+
+# TEST SHAPE
+def test_shape_null():
+    a = None
+    assert m.shape(a) == (0, 0)
+    
+def test_shape_empty():
+    a = [[]]
+    assert m.shape(a) == (0, 0)
+    
+def test_shape_row():
+    a = [[1, 2, 3]]
+    assert m.shape(a) == (1, 3)
+    
+def test_shape_column():
+    a = [[1], [2], [3]]
+    assert m.shape(a) == (3, 1)
+    
+def test_shape_matrix():
+    a = [[1, 2, 3], [1, 1, 1], [1, 1, 1]]
+    assert m.shape(a) == (3, 3)
+
+
+# TEST GET_ROW
+def test_get_row_null():
+    a = None
+    with pytest.raises(AssertionError, match=r'.*pass a matrix.*'):
+        m.get_row(a)
+    
+def test_get_row_empty():
+    a = [[]]
+    assert m.get_row(a, 0) == []
+    
+def test_get_row_oob():
+    a = [[]]
+    with pytest.raises(AssertionError, match=r'.*out of bounds.*'):
+        m.get_row(a, 1)
+    
+def test_get_row_row():
+    a = [[1, 2, 3]]
+    assert m.get_row(a, 0) == [1, 2, 3]
+    
+def test_get_row_column():
+    a = [[1], [2], [3]]
+    assert m.get_row(a, 1) == [2]
+    
+def test_get_row_matrix():
+    a = [[1, 2, 3], [1, 1, 1], [1, 1, 1]]
+    assert m.get_row(a, 2) == [1, 1, 1]
+    
+def test_get_row_generator():
+    a = [[1, 2, 3], [1, 1, 1], [1, 1, 1]]
+    assert list(m.get_row(a)) == a
+    
+    
+# TEST GET_COLUMN
+def test_get_column_null():
+    a = None
+    with pytest.raises(AssertionError, match=r'.*pass a matrix.*'):
+        m.get_column(a)
+    
+def test_get_column_empty():
+    a = [[]]
+    assert m.get_column(a, 0) == []
+    
+def test_get_column_oob():
+    a = [[1], [2], [3]]
+    with pytest.raises(AssertionError, match=r'.*out of bounds.*'):
+        m.get_column(a, 1)
+    
+def test_get_column_row():
+    a = [[1, 2, 3]]
+    assert m.get_column(a, 0) == [1]
+    
+def test_get_column_column():
+    a = [[1], [2], [3]]
+    assert m.get_column(a, 0) == [1, 2, 3]
+    
+def test_get_column_matrix():
+    a = [[1, 2, 3], [1, 1, 1], [1, 1, 1]]
+    assert m.get_column(a, 2) == [3, 1, 1]
+    
+def test_get_column_generator():
+    a = [[1, 2, 3], [1, 1, 1]]
+    test = list(m.get_column(a))
+    assert test[2] == [3, 1]
+
+
+# TEST BUILD MATRIX
+def test_build_matrix_zero():
+    def zero(i, j):
+        return 0
+    assert m.build_matrix(1, 1, zero) == [[0]]
+
+def test_build_matrix_zeroes():
+    def zero(i, j):
+        return 0
+    assert m.build_matrix(2, 3, zero) == [[0, 0, 0], [0, 0, 0]]
+    
+def test_build_matrix_function():
+    def add_function(i, j):
+        return i + j
+    assert m.build_matrix(2, 3, add_function) == (
+        [[0, 1, 2], [1, 2, 3]])
+    
+
+# TEST IDENTITY MATRIX
+def test_identify_matrix_null():
+    assert m.identity_matrix(0) == [[]]
+
+def test_identify_matrix_1():
+    assert m.identity_matrix(1) == [[1]]
+
+def test_identify_matrix_2():
+    assert m.identity_matrix(2) == [[1, 0], [0, 1]]
---
+++ tests/test_vector.py
@@ -0,0 +1,262 @@
+import math
+import pytest
+
+from data_tools.linear_algebra import vector as v
+
+# VECTOR ADD TESTS
+def test_add_unequal():
+    a = [1, 2]
+    b = [1]
+    with pytest.raises(AssertionError, match=r'.*equal size.*'):
+        v.add(a, b)
+
+def test_add_positive():
+    a = [1, 2]
+    b = [3, 4]
+    assert v.add(a, b) == [4, 6]
+
+def test_add_negative():
+    a = [-1, 2]
+    b = [3, -4]
+    assert v.add(a, b) == [2, -2]
+
+def test_add_zero():
+    a = [1, 2]
+    b = [0, 0]
+    assert v.add(a, b) == [1, 2]
+    
+def test_add_self():
+    a = [1, 2]
+    assert v.add(a, a) == [2, 4]
+
+
+# VECTOR SUBTRACT TESTS
+def test_subtract_unequal():
+    a = [1, 2]
+    b = [1]
+    with pytest.raises(AssertionError, match=r'.*equal size.*'):
+        v.subtract(a, b)
+
+def test_subtract_positive():
+    a = [1, 2]
+    b = [3, 4]
+    assert v.subtract(a, b) == [-2, -2]
+
+def test_subtract_negative():
+    a = [-1, 2]
+    b = [3, -4]
+    assert v.subtract(a, b) == [-4, 6]
+
+def test_subtract_zero():
+    a = [1, 2]
+    b = [0, 0]
+    assert v.subtract(a, b) == [1, 2]
+    
+def test_subtract_self():
+    a = [1, 2]
+    assert v.subtract(a, a) == [0, 0]
+    
+    
+# VECTOR SUM TESTS
+def test_vector_sum_empty():
+    vectors = []
+    with pytest.raises(AssertionError, match=r'.*list of vectors*'):
+        v.vector_sum(vectors)
+    
+def test_vector_sum_unequal():
+    a = [1, 2]
+    b = [1]
+    vectors = [a, b]
+    with pytest.raises(AssertionError, match=r'.*equal size.*'):
+        v.vector_sum(vectors)
+
+def test_vector_sum_positive():
+    a = [1, 2]
+    b = [3, 4]
+    vectors = [a, b]
+    assert v.vector_sum(vectors) == v.add(a, b)
+
+def test_vector_sum_negative():
+    a = [-1, 2]
+    b = [3, -4]
+    vectors = [a, b]
+    assert v.vector_sum(vectors) == v.add(a, b)
+
+def test_vector_sum_zero():
+    a = [1, 2]
+    b = [0, 0]
+    vectors = [a, b]
+    assert v.vector_sum(vectors) == v.add(a, b)
+    
+def test_vector_sum_self():
+    a = [1, 2]
+    vectors = [a, a]
+    assert v.vector_sum(vectors) == v.add(a, a)
+    
+def test_vector_sum_many():
+    a = [1, 1, 1]
+    b = [2, 3, 4]
+    c = [0, 0, 0]
+    d = [-1, -1, -1]
+    vectors = [a, b, c, d]
+    assert v.vector_sum(vectors) == [2, 3, 4]
+
+
+# VECTOR MULTIPLY TESTS
+def test_multiply_default():
+    a = [1, 1]
+    assert v.scalar_multiply(a) == a
+
+def test_multiply_positive():
+    c = 2
+    a = [1, 2]
+    assert v.scalar_multiply(a, c) == [2, 4]
+
+def test_multiply_negative():
+    c = -2
+    a = [1, 2]
+    assert v.scalar_multiply(a, c) == [-2, -4]
+
+def test_multiply_zero():
+    c = 0
+    a = [1, 2]
+    assert v.scalar_multiply(a, c) == [0, 0]
+    
+    
+# VECTOR MEAN TESTS
+def test_vector_mean_empty():
+    vectors = []
+    with pytest.raises(AssertionError, match=r'.*list of vectors*'):
+        v.vector_mean(vectors)
+    
+def test_vector_mean_unequal():
+    a = [1, 2]
+    b = [1]
+    vectors = [a, b]
+    with pytest.raises(AssertionError, match=r'.*equal size.*'):
+        v.vector_mean(vectors)
+
+def test_vector_mean_positive():
+    a = [1, 2]
+    b = [3, 4]
+    vectors = [a, b]
+    assert v.vector_mean(vectors) == [2, 3]
+
+def test_vector_mean_negative():
+    a = [-1, 2]
+    b = [3, -4]
+    vectors = [a, b]
+    assert v.vector_mean(vectors) == [1, -1]
+
+def test_vector_mean_zero():
+    a = [1, 2]
+    b = [0, 0]
+    vectors = [a, b]
+    assert v.vector_mean(vectors) == [0.5, 1]
+    
+def test_vector_mean_self():
+    a = [1, 2]
+    vectors = [a, a]
+    assert v.vector_mean(vectors) == a
+    
+def test_vector_mean_many():
+    a = [1, 1, 1]
+    b = [2, 3, 4]
+    c = [0, 0, 0]
+    d = [-1, -1, -1]
+    vectors = [a, b, c, d]
+    assert v.vector_mean(vectors) == [0.5, 0.75, 1]
+    
+
+# DOT PRODUCT TESTS
+def test_dot_unequal():
+    a = [1, 2]
+    b = [1]
+    with pytest.raises(AssertionError, match=r'.*equal size.*'):
+        v.dot(a, b)
+
+def test_dot_positive():
+    a = [1, 2]
+    b = [3, 4]
+    assert v.dot(a, b) == 11
+
+def test_dot_negative():
+    a = [-1, 2]
+    b = [3, -4]
+    assert v.dot(a, b) == -11
+
+def test_dot_zero():
+    a = [1, 2]
+    b = [0, 0]
+    assert v.dot(a, b) == 0
+    
+def test_dot_self():
+    a = [1, 2]
+    assert v.dot(a, a) == 5
+    
+    
+# TEST SUM OF SQUARES
+def test_sum_of_squares_zero():
+    a = [0, 0]
+    assert v.sum_of_squares(a) == 0
+    
+def test_sum_of_squares_one():
+    a = [1, 1]
+    assert v.sum_of_squares(a) == 2
+    
+def test_sum_of_squares_positive():
+    a = [1, 2, 3]
+    assert v.sum_of_squares(a) == 14
+    
+def test_sum_of_squares_negative():
+    a = [-1, -2, -3]
+    assert v.sum_of_squares(a) == 14
+    
+    
+# TEST MAGNITUDE
+def test_magnitude_zero():
+    a = [0, 0]
+    assert v.magnitude(a) == 0
+    
+def test_magnitude_one():
+    a = [1, 1]
+    assert v.magnitude(a) == math.sqrt(2)
+    
+def test_magnitude_positive():
+    a = [1, 2, 3]
+    assert v.magnitude(a) == math.sqrt(14)
+    
+def test_magnitude_negative():
+    a = [-1, -2, -3]
+    assert v.magnitude(a) == math.sqrt(14)
+    
+def test_magnitude_float():
+    a = [1, 0.5, 0]
+    assert v.magnitude(a) == math.sqrt(1.25)
+    
+    
+# TEST DISTANCE
+def test_distance_zero():
+    a = [0, 0]
+    b = [0, 0]
+    assert v.distance(a, b) == 0
+    
+def test_distance_zero_one():
+    a = [1, 1]
+    b = [0, 0]
+    assert v.distance(a, b) == math.sqrt(2)
+    
+def test_distance_positive():
+    a = [1, 2, 3]
+    b = [3, 2, 1]
+    assert v.distance(a, b) == math.sqrt(8)
+    
+def test_distance_negative():
+    a = [-1, -2, -3]
+    b = [3, 2, 1]
+    assert v.distance(a, b) == math.sqrt(48)
+    
+def test_distance_float():
+    a = [1, 0.5, 0]
+    b = [0, 0, 0]
+    assert v.distance(a, b) == math.sqrt(1.25)
