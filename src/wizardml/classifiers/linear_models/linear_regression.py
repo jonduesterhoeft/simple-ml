de01e5e20896edde6ac7f16d9765fffd8a97dae3
Jon Duesterhoeft <jon.duesterhoeft@gmail.com>
2023-06-20 20:27:55 -0500

    documentation and testing for linear_regression

--- src/wizardml/classifiers/linear_models/linear_regression.py
+++ src/wizardml/classifiers/linear_models/linear_regression.py
@@ -5,38 +5,128 @@ from ...math.stats.stats import corr, std, mean, subtract_mean
 
 def predict(alpha: float, beta: float, x_i: float) -> float:
     """
-    Calculates the distance between two vectors v and w.
+    Predicts using a simple linear regression model.
+    y_i = beta*x_i + alpha + error_i
     
     Parameters
     ----------
-    v : Vector
-        A Vector of type List[float].
-    w : Vector
-        A Vector of type List[float].
+    alpha : float
+        alpha term for simple linear regression.
+    beta : float
+        beta term for simple linear regression.
+    x_i : float
+        The value of x for which we want to predict y.
 
     Returns
     -------
     float
-        Returns the distance between two vectors v and w.
+        Returns the predicted value of y at x.
     """
     return beta * x_i + alpha
 
 def error(alpha: float, beta: float, x_i: float, y_i: float) -> float:
+    """
+    Calculates the error of the prediction against a known actual.
+    
+    Parameters
+    ----------
+    alpha : float
+        alpha term for simple linear regression.
+    beta : float
+        beta term for simple linear regression.
+    x_i : float
+        The value of x for which we want to predict y.
+    y_i : float
+        The known value of y at x.
+
+    Returns
+    -------
+    float
+        The error in prediction as y_predicted - y_actual
+    """
     return predict(alpha, beta, x_i) - y_i
 
 def sum_of_squares_error(alpha: float, beta: float, x: Vector, y: Vector) -> float:
+    """
+    Calculates the sum of squared errors from a simple linear regression.
+    
+    Parameters
+    ----------
+    alpha : float
+        alpha term for simple linear regression.
+    beta : float
+        beta term for simple linear regression.
+    x : Vector
+        The values of x for which we want to predict values.
+    y : Vector
+        The actual values y for the given x.
+
+    Returns
+    -------
+    float
+        The sum of squared error for a simple linear regression
+    """
     return sum(error(alpha, beta, x_i, y_i) ** 2 for x_i, y_i in zip(x, y))
 
 def fit_least_squares(x: Vector, y: Vector) -> Tuple[float, float]:
+    """
+    Calculates the alpha, beta terms for simple linear regression
+    that minimizes the sum of squared errors
+    
+    Parameters
+    ----------
+    x : Vector
+        The values of x for which we want to predict values.
+    y : Vector
+        The actual values y for the given x.
+
+    Returns
+    -------
+    alpha, beta : float, float
+        The alpha, beta terms that minimize the sum of squared errors
+    """
     beta = corr(x, y) * std(y) / std(x)
     alpha = mean(y) - beta * mean(x)
     return alpha, beta
 
 def total_sum_of_squares(y: Vector) -> float:
+    """
+    The total sum of variations of y_i's from their mean.
+    
+    Parameters
+    ----------
+    y : Vector
+        fA vector of y_i's.
+
+    Returns
+    -------
+    float
+        The sum of squared values of y_i's with their mean subtracted.
+    """
     return sum(v ** 2 for v in subtract_mean(y))
 
 def r_squared(alpha: float, beta: float, x: Vector, y: Vector) -> float:
+    """
+    The fraction of variation in y explained by the model.
+    
+    Parameters
+    ----------
+    alpha : float
+        alpha term for simple linear regression.
+    beta : float
+        beta term for simple linear regression.
+    x : Vector
+        The values of x for which we want to predict values.
+    y : Vector
+        The actual values y for the given x.
+
+    Returns
+    -------
+    float
+        The value calculated for R^2.
+    """
     return 1.0 - (sum_of_squares_error(alpha, beta, x, y) / total_sum_of_squares(y))
+
 
 if __name__ == '__main__':
     pass
---
+++ tests/test_linear_regression.py
@@ -0,0 +1,63 @@
+import math
+import pytest
+
+from src.wizardml.classifiers.linear_models import linear_regression as l
+
+# TEST PREDICT
+def test_predict():
+    alpha = 2.0
+    beta = 0.5
+    x_i = 10.0
+    expected_result = 7.0
+    result = l.predict(alpha, beta, x_i)
+    assert pytest.approx(result) == expected_result
+    
+# TEST ERROR
+def test_error():
+    alpha = 2.0
+    beta = 0.5
+    x_i = 10.0
+    y_i = 5.0
+    expected_result = 2.0
+    result = l.error(alpha, beta, x_i, y_i)
+    assert pytest.approx(result) == expected_result
+
+# TEST SUM_OF_SQUARES_ERROR
+def test_sum_of_squares_error():
+    alpha = 2.0
+    beta = 0.5
+    x = [1.0, 2.0, 3.0, 4.0]
+    y = [3.0, 4.0, 5.0, 6.0]
+    expected_result = 7.5
+    result = l.sum_of_squares_error(alpha, beta, x, y)
+    assert pytest.approx(result, abs=1e-5) == expected_result
+    
+# TEST FIT_LEAST_SQUARES
+def test_fit_least_squares():
+    x = [1.0, 2.0, 3.0, 4.0]
+    y = [3.0, 4.0, 5.0, 6.0]
+    expected_result = (2.0, 1.0)
+    result = l.fit_least_squares(x, y)
+    assert pytest.approx(result[0]) == expected_result[0]
+    assert pytest.approx(result[1]) == expected_result[1]
+
+# TEST TOTAL_SUM_OF_SQUARES
+def test_total_sum_of_squares():
+    y = [1.0, 2.0, 3.0, 4.0]
+    expected_result = 5.0
+    result = l.total_sum_of_squares(y)
+    assert pytest.approx(result) == expected_result
+
+# TEST R_SQUARED
+def test_r_squared():
+    alpha = 2.0
+    beta = 0.5
+    x = [1.0, 2.0, 3.0, 4.0]
+    y = [3.0, 4.0, 5.0, 6.0]
+    expected_result = 0.9
+    result = l.r_squared(alpha, beta, x, y)
+    assert pytest.approx(result) == expected_result
+
+
+if __name__ == '__main__':
+    pass
