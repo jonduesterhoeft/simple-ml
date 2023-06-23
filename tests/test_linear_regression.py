1fec7e3fb944e63cf526086889d497c971e096f2
Jon Duesterhoeft <jon.duesterhoeft@gmail.com>
2023-06-22 22:03:46 -0500

    started updating unit tests

--- tests/test_linear_regression.py
+++ tests/test_linear_regression.py
@@ -5,67 +5,73 @@ from src.wizardml.classifiers.linear_models import linear_regression as l
 
 # TEST PREDICT
 def test_predict():
-    alpha = 2.0
-    beta = 0.5
-    x_i = 10.0
-    expected_result = 7.0
-    result = l.predict(alpha, beta, x_i)
+    x = [1.0, 2.0, 3.0]
+    beta = [0.5, 0.25, 0.75]
+    expected_result = 3.25
+    result = l.predict(x, beta)
     assert pytest.approx(result) == expected_result
     
 # TEST ERROR
 def test_error():
-    alpha = 2.0
-    beta = 0.5
-    x_i = 10.0
-    y_i = 5.0
-    expected_result = 2.0
-    result = l.error(alpha, beta, x_i, y_i)
+    x = [1.0, 2.0, 3.0]
+    y = 5.0
+    beta = [0.5, 0.25, 0.75]
+    expected_result = -1.75
+    result = l.error(x, y, beta)
     assert pytest.approx(result) == expected_result
 
-# TEST SUM_OF_SQUARES_ERROR
-def test_sum_of_squares_error():
-    alpha = 2.0
-    beta = 0.5
-    x = [1.0, 2.0, 3.0, 4.0]
-    y = [3.0, 4.0, 5.0, 6.0]
-    expected_result = 7.5
-    result = l.sum_of_squares_error(alpha, beta, x, y)
-    assert pytest.approx(result, abs=1e-5) == expected_result
+# TEST SQUARED_ERROR
+def test_squared_error():
+    x = [1.0, 2.0, 3.0]
+    y = 5.0
+    beta = [0.5, 0.25, 0.75]
+    expected_result = [-3.5, -7, -10.5]
+    result = l.squared_error(x, y, beta)
+    assert pytest.approx(result) == expected_result
     
+# TEST SQUARED_ERROR_GRADIENT
+def test_squared_error_gradient():
+    x = [1.0, 2.0, 3.0]
+    y = 5.0
+    beta = [0.5, 0.25, 0.75]
+    expected_result = 3.0625
+    result = l.squared_error(x, y, beta)
+    assert pytest.approx(result) == expected_result
+
 # TEST FIT_LEAST_SQUARES
-def test_fit_least_squares():
-    x = [1.0, 2.0, 3.0, 4.0]
-    y = [3.0, 4.0, 5.0, 6.0]
-    expected_result = (2.0, 1.0)
-    result = l.fit_least_squares(x, y)
-    assert pytest.approx(result[0]) == expected_result[0]
-    assert pytest.approx(result[1]) == expected_result[1]
+# def test_fit_least_squares():
+    # x = [1.0, 2.0, 3.0, 4.0]
+    # y = [3.0, 4.0, 5.0, 6.0]
+    # expected_result = (2.0, 1.0)
+    # result = l.fit_least_squares(x, y)
+    # assert pytest.approx(result[0]) == expected_result[0]
+    # assert pytest.approx(result[1]) == expected_result[1]
 
 # TEST TOTAL_SUM_OF_SQUARES
-def test_total_sum_of_squares():
-    y = [1.0, 2.0, 3.0, 4.0]
-    expected_result = 5.0
-    result = l.total_sum_of_squares(y)
-    assert pytest.approx(result) == expected_result
+# def test_total_sum_of_squares():
+    # y = [1.0, 2.0, 3.0, 4.0]
+    # expected_result = 5.0
+    # result = l.total_sum_of_squares(y)
+    # assert pytest.approx(result) == expected_result
 
 # TEST R_SQUARED
-def test_r_squared():
-    alpha = 2.0
-    beta = 0.5
-    x = [1.0, 2.0, 3.0, 4.0]
-    y = [3.0, 4.0, 5.0, 6.0]
-    expected_result = -0.5
-    result = l.r_squared(alpha, beta, x, y)
-    assert pytest.approx(result) == expected_result
+# def test_r_squared():
+#     alpha = 2.0
+#     beta = 0.5
+#     x = [1.0, 2.0, 3.0, 4.0]
+#     y = [3.0, 4.0, 5.0, 6.0]
+#     expected_result = -0.5
+#     result = l.r_squared(alpha, beta, x, y)
+#     assert pytest.approx(result) == expected_result
     
-def test_r_squared_perfect_fit():
-    alpha = 2.0
-    beta = 1.0
-    x = [1.0, 2.0, 3.0, 4.0]
-    y = [3.0, 4.0, 5.0, 6.0]
-    expected_result = 1.0
-    result = l.r_squared(alpha, beta, x, y)
-    assert pytest.approx(result) == expected_result
+# def test_r_squared_perfect_fit():
+#     alpha = 2.0
+#     beta = 1.0
+#     x = [1.0, 2.0, 3.0, 4.0]
+#     y = [3.0, 4.0, 5.0, 6.0]
+#     expected_result = 1.0
+#     result = l.r_squared(alpha, beta, x, y)
+#     assert pytest.approx(result) == expected_result
 
 
 if __name__ == '__main__':
